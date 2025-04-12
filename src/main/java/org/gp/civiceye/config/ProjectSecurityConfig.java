8b3af5ffe0acd81b39debceb50d8337030bbf2a2
yousef hesham <eng-yousef-hesham@hotmail.com>
2025-04-12 05:31:32 +0200

    fix: update /check endpoint requestMatchers in ProjectSecurityConfig
    refactor: change response for invalid JWT token

--- src/main/java/org/gp/civiceye/config/ProjectSecurityConfig.java
+++ src/main/java/org/gp/civiceye/config/ProjectSecurityConfig.java
@@ -73,11 +73,12 @@ public class ProjectSecurityConfig {
                 .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/V1/masteradmins","/api/V1/admin","/api/V1/citie/"
-                                ,"api/V1/cities/","api/V1/departments","api/V1/governorates","api/V1/check").hasRole("MASTERADMIN")
-                        .requestMatchers("/api/V1/governorateadmins","api/V1/check").hasRole("GOVERNORATEADMIN")
-                        .requestMatchers("/api/V1/cityadmins","api/V1/check").hasRole("CITYADMIN")
-                        .requestMatchers("/api/V1/employees","api/V1/check").hasRole("EMPLOYEE")
-                        .requestMatchers("/api/V1/citizen","api/V1/check").hasRole("CITIZEN")
+                                ,"api/V1/cities/","api/V1/departments","api/V1/governorates").hasRole("MASTERADMIN")
+                        .requestMatchers("/api/V1/governorateadmins").hasRole("GOVERNORATEADMIN")
+                        .requestMatchers("/api/V1/cityadmins").hasRole("CITYADMIN")
+                        .requestMatchers("/api/V1/employees").hasRole("EMPLOYEE")
+                        .requestMatchers("/api/V1/citizen").hasRole("CITIZEN")
+                        .requestMatchers("/api/V1/check").hasAnyRole("MASTERADMIN", "GOVERNORATEADMIN", "CITYADMIN", "EMPLOYEE", "CITIZEN")
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/api/V1/login").permitAll()
 //                        .requestMatchers(HttpMethod.POST, "/api/V1/admin").permitAll() // Explicitly allowing POST
                         .anyRequest().authenticated()
