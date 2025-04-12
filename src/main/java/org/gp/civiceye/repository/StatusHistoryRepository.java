a1d1ed8d04f93980cd5e5f3dc0c747d682d3e70b
mohammedArabie <94062217+mohammedArabie@users.noreply.github.com>
2025-04-12 04:48:49 +0200

    Initialize report API (#1)
    * refactor: optimize database feat: added GovernorateAdmincontroller and MasterAdminController added login for citizen and employee
    
    * inialize report API
    
    ---------
    
    Co-authored-by: yousef hesham <eng-yousef-hesham@hotmail.com>

--- src/main/java/org/gp/civiceye/repository/StatusHistoryRepository.java
+++ src/main/java/org/gp/civiceye/repository/StatusHistoryRepository.java
@@ -1,9 +1,14 @@
 package org.gp.civiceye.repository;
 
+import org.gp.civiceye.repository.entity.Report;
 import org.gp.civiceye.repository.entity.StatusHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StatusHistoryRepository extends JpaRepository<StatusHistory, Long> {
+    Optional<StatusHistory> findTopByReportOrderByStartTimeDesc(Report report);
+
 }
--- src/main/java/org/gp/civiceye/service/impl/ReportServiceImpl.java
+++ src/main/java/org/gp/civiceye/service/impl/ReportServiceImpl.java
@@ -1,26 +1,108 @@
 package org.gp.civiceye.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.gp.civiceye.mapper.CreateReportDTO;
 import org.gp.civiceye.mapper.ReportDTO;
-import org.gp.civiceye.repository.EmployeeRepository;
-import org.gp.civiceye.repository.ReportRepository;
-import org.gp.civiceye.repository.entity.Employee;
+import org.gp.civiceye.mapper.UpdateReportStatusDTO;
+import org.gp.civiceye.repository.*;
+import org.gp.civiceye.repository.entity.*;
 import org.gp.civiceye.service.ReportService;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class ReportServiceImpl implements ReportService {
 
     ReportRepository reportRepository;
+    CityRepository cityRepository;
+    CitizenRepository citizenRepository;
+    EmployeeRepository employeeRepository;
+    StatusHistoryRepository statusHistoryRepository;
 
 
-    public ReportServiceImpl(ReportRepository reportRepository) {
+    public ReportServiceImpl(ReportRepository reportRepository,CityRepository cityRepository, CitizenRepository citizenRepository,EmployeeRepository employeeRepository ,StatusHistoryRepository statusHistoryRepository) {
         this.reportRepository = reportRepository;
+        this.cityRepository = cityRepository;
+        this.citizenRepository = citizenRepository;
+        this.employeeRepository = employeeRepository;
+        this.statusHistoryRepository = statusHistoryRepository;
+    }
+
+    public Long submitReport(CreateReportDTO dto) {
+        City city = cityRepository.findById(dto.getCityId())
+                .orElseThrow(() -> new EntityNotFoundException("City not found with ID: " + dto.getCityId()));
+
+        Citizen citizen = citizenRepository.findById(dto.getCitizenId())
+                .orElseThrow(() -> new EntityNotFoundException("Citizen not found with ID: " + dto.getCitizenId()));
+
+        Optional<List<Employee>> employeesOpt = employeeRepository.findByCity(city);
+        if (employeesOpt.isEmpty() || employeesOpt.get().isEmpty()) {
+            throw new EntityNotFoundException("No employees available for city: " + city.getName());
+        }
+
+        Employee assignedEmployee = employeesOpt.get().get(0);
+
+        Report report = dto.toReportEntity(city,citizen,assignedEmployee);
+        reportRepository.save(report);
+
+        StatusHistory initialStatus = StatusHistory.builder()
+                .status(ReportStatus.Submitted)
+                .notes("Initial status")
+                .startTime(LocalDateTime.now())
+                .endTime(null)
+                .report(report)
+                .changedByEmployee(assignedEmployee)
+                .build();
+
+        statusHistoryRepository.save(initialStatus);
+        return report.getReportId();
+    }
+
+    @Override
+    public List<ReportDTO> getReportsForUser(Long userId) {
+        return reportRepository.findAllByCitizen_CitizenId(userId).stream()
+                .map(ReportDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ReportDTO> getReportsForEmployee(Long employeeId) {
+        return reportRepository.findAllByAssignedEmployee_EmpId(employeeId).stream()
+                .map(ReportDTO::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public void updateReportStatus(UpdateReportStatusDTO dto) {
+        Report report = reportRepository.findById(dto.getReportId())
+                .orElseThrow(() -> new RuntimeException("Report not found"));
+
+        Employee employee = employeeRepository.findById(dto.getEmployeeId())
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+
+        StatusHistory lastStatus = statusHistoryRepository.findTopByReportOrderByStartTimeDesc(report)
+                .orElseThrow(() -> new RuntimeException("No status history found"));
+
+        lastStatus.setEndTime(LocalDateTime.now());
+        statusHistoryRepository.save(lastStatus);
+
+        StatusHistory newStatus = StatusHistory.builder()
+                .report(report)
+                .status(dto.getNewStatus())
+                .startTime(LocalDateTime.now())
+                .endTime(null)
+                .changedByEmployee(employee)
+                .notes(dto.getNotes() != null ? dto.getNotes() : "")
+                .build();
+
+        statusHistoryRepository.save(newStatus);
+
+        report.setCurrentStatus(dto.getNewStatus());
+        report.setUpdatedAt(LocalDateTime.now());
+        reportRepository.save(report);
     }
 
 
