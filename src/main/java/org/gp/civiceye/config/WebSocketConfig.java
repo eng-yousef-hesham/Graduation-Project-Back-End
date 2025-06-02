dae9405806dd8cbe71da1bdbf34709f294ea83fc
eng-yousef-hesham <Eng_yousef_hesham@hotmail.com>
2025-06-02 14:14:31 +0300

    feat: add websocket configuration

---
+++ src/main/java/org/gp/civiceye/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package org.gp.civiceye.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:3000/",
+                "https://graduation-front-1.onrender.com",
+                "http://localhost:8000/").withSockJS(); // Web & Flutter support
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
--- src/main/java/org/gp/civiceye/controller/ReportAnalysisController.java
+++ src/main/java/org/gp/civiceye/controller/ReportAnalysisController.java
@@ -1,11 +1,17 @@
 package org.gp.civiceye.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.gp.civiceye.mapper.report.CreateReportDTO;
+import org.gp.civiceye.mapper.report.ReportDTO;
 import org.gp.civiceye.service.ReportAnalysisService;
 import org.gp.civiceye.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +22,12 @@ import java.util.Map;
 @RequestMapping("/api/V1/reports/analysis")
 public class ReportAnalysisController {
     private final ReportAnalysisService reportAnalysisService;
+    private final ReportService reportService;
 
     @Autowired
-    public ReportAnalysisController(ReportAnalysisService reportAnalysisService) {
+    public ReportAnalysisController(ReportAnalysisService reportAnalysisService,ReportService reportService) {
         this.reportAnalysisService = reportAnalysisService;
+        this.reportService = reportService;
     }
 
 
@@ -40,6 +48,18 @@ public class ReportAnalysisController {
     @Operation(summary = "Geographical Distribution of Reports (Count by City)" )
     public ResponseEntity<List<Map<String, Object>>> getReportsPerCity() {
         return ResponseEntity.ok(reportAnalysisService.getReportsPerCity());
+    }
+
+    @MessageMapping("/send")
+    @SendTo("/topic/messages")
+    public ResponseEntity<List<ReportDTO>> send(@RequestBody CreateReportDTO dto) {
+        Long reportId = reportService.submitReport(dto);
+
+        List<ReportDTO> reports = reportService.GetAllReports();
+        if (reports.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(reports, HttpStatus.OK);
     }
 
 }
